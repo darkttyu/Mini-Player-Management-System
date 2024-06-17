abeda069a3e7cc7f01f7f87e72a3249c4797f54b
darkttyu <ctrlfrz0710@gmail.com>
2024-06-17 17:51:28 +0800

    Inserted Comments for Code Readability

--- project_database.py
+++ project_database.py
@@ -1,5 +1,6 @@
 import mysql.connector
 
+# Establishing a connection to MySQL database
 mydb = mysql.connector.connect(
     host='localhost',
     user='root',
@@ -8,15 +9,24 @@ mydb = mysql.connector.connect(
     database='player_management'
 )
 
+# Creating a cursor object to execute SQL queries
 cursor = mydb.cursor()
 
+
+# Function to insert a new player into the database
 def insert_player(playername, age, role, winrate, recentlyused, mostused, teamparticipation, teamid):
     insert_formula = ("INSERT INTO players (playername, age, role, win_rate, recently_used, most_used, "
-                  "team_participation, teamID) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)")
+                      "team_participation, teamID) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)")
     player = (playername, age, role, winrate, recentlyused, mostused, teamparticipation, teamid)
+
+    # Executing the SQL INSERT statement
     cursor.execute(insert_formula, player)
+
+    # Committing the transaction to apply changes to the database
     mydb.commit()
 
+
+# Function to retrieve player information from the database
 def retrieve_player(name):
     read_formula = ("SELECT playerName, age, role, "
                     "win_rate, recently_used, most_used, "
@@ -24,10 +34,15 @@ def retrieve_player(name):
                     "FROM PLAYERS INNER JOIN "
                     "TEAMS ON PLAYERS.TEAMID = TEAMS.TEAMID "
                     "WHERE playername = %s")
+
+    # Executing the SQL SELECT statement
     cursor.execute(read_formula, (name,))
+
+    # Fetching the first row of the result
     player_data = cursor.fetchone()
     pdata_list = list(player_data)
 
+    # Printing retrieved player information
     print("\nRetrieved Data from Database")
     player_info = f"""
     Player Name: {pdata_list[0]}
@@ -42,12 +57,17 @@ def retrieve_player(name):
     """
     print(player_info)
 
+
+# Function to update player information in the database
 def update_player(name, column):
     column_list = ['playerName', 'age', 'role', 'win_rate', 'recent_used', 'most_used', 'team_participation']
 
+    # Checking if the specified column is valid
     if column not in column_list:
         print("Invalid Column")
         return
+
+    # Handling different data types for update
     elif column == 'win_rate' or column == 'team_participation':
         new_value = float(input("Enter New Value: "))
     elif column == 'age':
@@ -55,15 +75,36 @@ def update_player(name, column):
     else:
         new_value = input("Enter New Value: ")
 
+    # Constructing the SQL UPDATE statement
     update_formula = ("UPDATE PLAYERS "
                       "SET " + column + " = %s "
-                      "WHERE playerName = %s")
+                                        "WHERE playerName = %s")
+
+    # Executing the SQL UPDATE statement
     cursor.execute(update_formula, (new_value, name))
     print("Table Updated Successfully!")
+
+    # Committing the transaction to apply changes to the database
     mydb.commit()
 
+
+# Function to delete a player from the database
 def delete_player(name):
     delete_formula = ("DELETE FROM PLAYERS WHERE playerName = %s")
+
+    # Executing the SQL DELETE statement
     cursor.execute(delete_formula, (name,))
     print("Player Deleted Successfully!")
+
+    # Committing the transaction to apply changes to the database
     mydb.commit()
+
+
+cursor.close()
+mydb.close()
+
+# The following code shows how to use the above functions to interact with the database:
+# - insert_playerinfo()
+# - read_playerinfo()
+# - update_playerinfo()
+# - delete_playerinfo()
--- project_main.py
+++ project_main.py
@@ -1,9 +1,10 @@
-import project_database as pdb
+import project_database as pdb  # Importing the database functions from project_database module
 
 
 class Player:
     def __init__(self, playername=None, age=None, role=None, winrate=None,
                  recentlyused=None, mostused=None, teamparticipation=None, teamid=None):
+        # Initialize Player object with optional attributes
         self.playername = playername
         self.age = age
         self.role = role
@@ -14,33 +15,40 @@ class Player:
         self.teamid = teamid
 
     def new_player(self):
+        # Method to insert a new player into the database
         pdb.insert_player(self.playername, self.age, self.role, self.winrate,
                           self.recentlyused, self.mostused, self.teamparticipation, self.teamid)
 
     @staticmethod
     def read_player(playername):
+        # Static method to retrieve player information from the database
         pdb.retrieve_player(playername)
 
     @staticmethod
     def update_player(playername, table_column):
+        # Static method to update player information in the database
         pdb.update_player(playername, table_column)
 
     @staticmethod
     def delete_player(playername):
+        # Static method to delete a player from the database
         pdb.delete_player(playername)
 
 
 class Team:
     def __init__(self, teamid, teamname, recent_match):
+        # Initialize Team object with attributes
         self.teamid = teamid
         self.teamname = teamname
         self.recent_match = recent_match
 
     def new_team(self):
+        # Method to handle creating a new team (currently not implemented)
         pass
 
-'''
+
 def insert_playerinfo():
+    # Function to interactively insert player data into the database
     print("Inserting Data to Database")
 
     player_name = input("Player Name: ")
@@ -51,44 +59,47 @@ def insert_playerinfo():
     player_mostused = input("Most Used: ")
     player_teamparticipation = float(input("Team Participation: "))
     player_teamid = int(input("Team ID: "))
+
+    # Create Player object and insert data into the database
     player = Player(player_name, player_age, player_role, player_winrate,
                     player_recentlyused, player_mostused, player_teamparticipation, player_teamid)
     player.new_player()
-'''
+
 
 def read_playerinfo():
+    # Function to interactively read player data from the database
     print("Reading Data From Database")
     read_playername = input("Enter Name to be Read: ")
+
+    # Read player information from the database
     player = Player()
     player.read_player(read_playername)
 
 
 def update_playerinfo():
+    # Function to interactively update player data in the database
     print("Updating Player Data from Database")
 
     name = input("Enter Player Name to be Updated: ")
     column = input("Enter Column to be Updated in Players Table: ")
+
+    # Update player information in the database
     player = Player()
     player.update_player(name, column)
 
 
 def delete_playerinfo():
+    # Function to interactively delete player data from the database
     print("Deleting Player Data from Database")
 
     name = input("Enter Player Name to be Deleted: ")
+
+    # Delete player from the database
     player = Player()
     player.delete_player(name)
 
-print("Inserting Data to Database")
-
-player_name = input("Player Name: ")
-player_age = int(input("Age: "))
-player_role = input("Role: ")
-player_winrate = float(input("Winrate: "))
-player_recentlyused = input("Recently Used: ")
-player_mostused = input("Most Used: ")
-player_teamparticipation = float(input("Team Participation: "))
-player_teamid = int(input("Team ID: "))
-player = Player(player_name, player_age, player_role, player_winrate,
-                player_recentlyused, player_mostused, player_teamparticipation, player_teamid)
-player.new_player()
+# The following code shows how to use the above functions to interact with the database:
+# - insert_playerinfo()
+# - read_playerinfo()
+# - update_playerinfo()
+# - delete_playerinfo()
